cbcbfa5235377e137642dea3af3ea75be70a85bf
insoLLLent <vitaly@lifanoff.ru>
2020-02-17 21:57:39 +0300

    added the USaveGameInstance class

---
+++ Source/ForgottenSigns/Public/Gameplay/SaveAndLoad/SaveGameInstance.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 Vitaly Lifanov <vitaly@lifanoff.ru>
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SaveGameInstance.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class FORGOTTENSIGNS_API USaveGameInstance : public UGameInstance {
+	GENERATED_BODY()
+
+};
