2e3bccf6aa672617c12d2f33ef2a28ef34cac23f
insoLLLent <vitaly@lifanoff.ru>
2020-02-17 22:36:19 +0300

    moved several variables from MainGameInstance to SaveGameInstance

--- Source/ForgottenSigns/Public/Gameplay/SaveAndLoad/SaveGameInstance.h
+++ Source/ForgottenSigns/Public/Gameplay/SaveAndLoad/SaveGameInstance.h
@@ -7,10 +7,18 @@
 #include "SaveGameInstance.generated.h"
 
 /**
- *
+ * Class for serialize saving
  */
-UCLASS()
+UCLASS(ClassGroup = "Gameplay|SaveAndLoad")
 class FORGOTTENSIGNS_API USaveGameInstance : public UGameInstance {
 	GENERATED_BODY()
 
+public:
+	// Standard lifetime of drop in seconds
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SaveGameInstance|Constants", meta = (Keyword = "drop lifetime"))
+		float dropLifetime = 900.0f;
+
+	// Z-Order for UI_Loader
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SaveGameInstance|Loader", meta = (Keyword = "ZOrder Z-Order UI_Loader"))
+		int32 zOrder = 1000;
 };
